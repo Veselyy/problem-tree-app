15887afbda3a57b5616e5ca717b121bf861aea6d
martinvesely <imthemartinveselyap@gmail.com>
2026-06-17 15:37:38 +0200

    add temporary tasks_db and get_nodes endpoint

--- main.py
+++ main.py
@@ -2,9 +2,29 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
+tasks_db = [
+    {
+        "id": 1,
+        "title": "Spuštění e-shopu",
+        "completed": False,
+        "parent_id": None
+    },
+    {
+        "id": 2,
+        "title": "Návrh databáze",
+        "completed": True,
+        "parent_id": 1  # Tento uzel patří pod uzel s ID 1
+    }
+]
+
 @app.route('/')
 def read_root():
     return jsonify({'message': 'Ahoj z Flasku!'})
 
+@app.route('/api/nodes', methods=['GET'])
+def get_nodes():
+    return jsonify(tasks_db)
+
 if __name__ == '__main__':
     app.run(debug=True, port=8000)
+
